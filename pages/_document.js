8160a2203ee63bb3f11f8c355d51c00307e5ae98
vimuths123 <vimuths@yahoo.com>
2023-09-05 20:27:47 +0530

    new

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <script type="text/javascript" async src="https://api.hypertarget.ai/"></script>
+      {/* <script type="text/javascript" async src="https://api.hypertarget.ai/"></script>
       <script
         type="text/javascript"
         dangerouslySetInnerHTML={{
@@ -26,7 +26,7 @@ export default function Document() {
               }
             `,
         }}
-      />
+      /> */}
       <body>
         <Main />
         <NextScript />
