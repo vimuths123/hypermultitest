d569f55d3b4a70e90c8fbf746ff1e90447034036
vimuths123 <vimuths@yahoo.com>
2023-09-05 21:42:49 +0530

    change

--- pages/_document.js
+++ pages/_document.js
@@ -4,27 +4,31 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <script type="text/javascript" async src="https://api.hypertarget.ai/"></script>
+      <script src="https://round-surf-9d28.pasimenzis.workers.dev/" type="text/javascript" />
       <script
-        type="text/javascript"
         dangerouslySetInnerHTML={{
           __html: `
-              function onloadFunction(){
-                HYPERSNIPPET.initURL(["d6936d43-1a4f-4802-b9cc-496562ab531e","c8911559-816a-43ba-8402-015d71a3a6b4"]);
-              }
-              window.onload = onloadFunction();
-              document.onclick = function (event) {
-                HYPERSNIPPET.analyzer(event,sessionStorage.getItem("key"),isLoadedForClick);
-              }
-              window.addEventListener("beforeunload", function(e) {
-                e.preventDefault();
-              });
-              const originalPush = window.history.pushState;
-              window.history.pushState = function() {
-                originalPush.apply(window.history, arguments);
-                HYPERSNIPPET.initURLSinglePage(["CODETOSITE","USERID"]);
-              }
-            `,
+                function onloadFunction() {
+                  HYPERSNIPPET.initURL(["websiteId","userId"]);
+                }
+                onloadFunction();
+
+
+                document.onclick = function (event) {
+                  HYPERSNIPPET.analyzer(event, sessionStorage.getItem("key"), isLoadedForClick);
+                }
+
+
+                window.addEventListener("beforeunload", function (e) {
+                  e.preventDefault();
+                })
+ 
+                const originalPush = window.history.pushState;
+                  window.history.pushState = function() {
+                  originalPush.apply(window.history, arguments);
+                  HYPERSNIPPET.initURLSinglePage(["websiteId","userId"]);
+                };
+              `,
         }}
       />
       <body>
