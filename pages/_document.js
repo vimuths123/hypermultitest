0f5651a3115b11e81d8670f9c6996218067ec102
vimuths123 <vimuths@yahoo.com>
2023-09-06 02:16:22 +0530

    New changes

--- pages/_document.js
+++ pages/_document.js
@@ -30,33 +30,9 @@ export default function Document() {
         }}
       /> */}
 
-      <Script src="https://round-surf-9d28.pasimenzis.workers.dev/" type="text/javascript" />
-      <Script id="hypertarget_track"
-        dangerouslySetInnerHTML={{
-          __html: `
-                function onloadFunction() {
-                  HYPERSNIPPET.initURL(["websiteId","userId"]);
-                }
-                onloadFunction();
+      
 
-
-                document.onclick = function (event) {
-                  HYPERSNIPPET.analyzer(event, sessionStorage.getItem("key"), isLoadedForClick);
-                }
-
-
-                window.addEventListener("beforeunload", function (e) {
-                  e.preventDefault();
-                })
- 
-                const originalPush = window.history.pushState;
-                  window.history.pushState = function() {
-                  originalPush.apply(window.history, arguments);
-                  HYPERSNIPPET.initURLSinglePage(["websiteId","userId"]);
-                };
-              `,
-        }}
-      />
+      
       <body>
         <Main />
         <NextScript />
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,64 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-   <p>Home</p>
+    <>
+      <Script src="https://round-surf-9d28.pasimenzis.workers.dev/" type="text/javascript" />
+      <Script id="my-script">{`
+      function onloadFunction() {
+        HYPERSNIPPET.initURL(["d6936d43-1a4f-4802-b9cc-496562ab531e","c8911559-816a-43ba-8402-015d71a3a6b4"]);
+      }
+      onloadFunction();
+
+
+      document.onclick = function (event) {
+        HYPERSNIPPET.analyzer(event, sessionStorage.getItem("key"), isLoadedForClick);
+      }
+
+
+      window.addEventListener("beforeunload", function (e) {
+        e.preventDefault();
+      })
+
+      const originalPush = window.history.pushState;
+        window.history.pushState = function() {
+        originalPush.apply(window.history, arguments);
+        HYPERSNIPPET.initURLSinglePage(["d6936d43-1a4f-4802-b9cc-496562ab531e","c8911559-816a-43ba-8402-015d71a3a6b4"]);
+      };
+      `}</Script>
+      {/* <Script id="hypertarget_track"
+        dangerouslySetInnerHTML={{
+          __html: `
+                alert('dds')
+                function onloadFunction() {
+                  HYPERSNIPPET.initURL(["d6936d43-1a4f-4802-b9cc-496562ab531e","c8911559-816a-43ba-8402-015d71a3a6b4"]);
+                }
+                onloadFunction();
+
+
+                document.onclick = function (event) {
+                  HYPERSNIPPET.analyzer(event, sessionStorage.getItem("key"), isLoadedForClick);
+                }
+
+
+                window.addEventListener("beforeunload", function (e) {
+                  e.preventDefault();
+                })
+ 
+                const originalPush = window.history.pushState;
+                  window.history.pushState = function() {
+                  originalPush.apply(window.history, arguments);
+                  HYPERSNIPPET.initURLSinglePage(["websiteId","userId"]);
+                };
+              `,
+        }}
+      /> */}
+      <p>Home</p>
+    </>
+
   )
 }
